428770151b6eef969b5148654163d110fe361b58
Dustin Horstmann <dustinhorstmann@Sunnys-MacBook-Pro.local>
2017-10-07 10:55:30 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+new Vue({
+  el: '#vue-app',
+  data: {
+    name: 'Dustin',
+    age: 36,
+    job: 'Developer',
+    website: 'http://dustinhorstmann.com',
+    someHTML: '<a href="//google.com">Some HTML</a>',
+    x: 0,
+    y: 0
+  },
+  methods: {
+    greet: function(time) {
+      // this refers to the Vue instance
+      // this.data.name can work, but not necessary
+      return 'Good ' + time + ', ' + this.name;
+    },
+    subtract: function(dec) {
+      this.age -= dec;
+    },
+    updateXY: function(e) {
+      this.x = e.screenX;
+      this.y = e.screenY;
+      //console.log(e);
+    }
+  }
+});
